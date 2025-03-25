5ad0d953f26bbb12f0418ebd15d07bca6eef3f27
Michael Geljic <michaelgeljic@gmail.com>
2025-03-25 18:42:26 +0100

    Add Apring Boot backend from Springinitializr

---
+++ src/test/java/com/rit/somnilog/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rit.somnilog.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
